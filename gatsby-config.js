13f2a078b4129563436f474625033c37e722f4f6
kimji <kimjigabk@gmail.com>
2023-05-09 16:01:21 -0400

    add page

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Github Project Compilation`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    author: `@kimjigabk`,
+    siteUrl: `https://github.com/kimjigabk`,
   },
   plugins: [
     `gatsby-plugin-image`,
---
+++ src/pages/page-3.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => {
+  return (
+    <Layout>
+      <h1> Hello from Third page</h1>
+    </Layout>
+  )
+}
+
+export default ThirdPage
